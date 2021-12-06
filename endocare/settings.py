b467b5aa835f3f896343ea0ee38193c72e25b7f2
thongle89 <thongle.aisolution@gmail.com>
2021-12-06 11:12:02 +0700

    change database

--- endocare/settings.py
+++ endocare/settings.py
@@ -76,23 +76,23 @@ WSGI_APPLICATION = 'endocare.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'postgres',
-#         'USER': 'thongle89',
-#         'PASSWORD':'646281T.l',
-#         'HOST':'database-2.chfm58xk1yl4.ap-southeast-1.rds.amazonaws.com',
-#         'PORT':'5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'thongle89',
+        'PASSWORD':'646281T.l',
+        'HOST':'database-2.chfm58xk1yl4.ap-southeast-1.rds.amazonaws.com',
+        'PORT':'5432',
+    }
+}
 
 
 # Password validation
