068608c88d78b6c732ce26ebde14f8259f5a8300
thongle89 <thongle.aisolution@gmail.com>
2021-12-09 08:41:12 +0700

    raspian_os

--- endocare/settings.py
+++ endocare/settings.py
@@ -159,7 +159,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'noreply.endocare@gmail.com'
-EMAIL_HOST_PASSWORD = 'endoservice2021'
+EMAIL_HOST_USER = 'noreply.noisoifujifilm@gmail.com'
+EMAIL_HOST_PASSWORD = 'endoserv!ce2021'
 
 # django_heroku.settings(locals())
