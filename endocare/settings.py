82f7332320bf8a413f77104fd804db226474f65f
thongle89 <thongle.aisolution@gmail.com>
2021-12-10 09:00:25 +0700

    setting_server

--- endocare/settings.py
+++ endocare/settings.py
@@ -23,7 +23,6 @@ ALLOWED_HOSTS = ['*.herokuapp.com',
 
 ]
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -70,43 +69,29 @@ TEMPLATES = [
         },
     },
 ]
-
 WSGI_APPLICATION = 'endocare.wsgi.application'
 
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-# DATABASES = {
-#      'default': {
-#          'ENGINE': 'django.db.backends.postgresql',
-#          'NAME': 'web_db',
-#          'USER': 'web_user',
-#          'PASSWORD':'nhakANDUkj81',
-#          'HOST':'localhost',
-#          'PORT':'5432',
-#      }
-#  }
-
-#
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'postgres',
-#         'USER': 'thongle89',
-#         'PASSWORD':'646281T.l',
-#         'HOST':'database-2.chfm58xk1yl4.ap-southeast-1.rds.amazonaws.com',
-#         'PORT':'5432',
-#     }
-# }
-
-
+     'default': {
+         'ENGINE': 'django.db.backends.postgresql',
+         'NAME': 'web_db',
+         'USER': 'web_user',
+         'PASSWORD':'nhakANDUkj81',
+         'HOST':'localhost',
+         'PORT':'5432',
+     }
+ }
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
@@ -139,7 +124,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 # PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
@@ -159,7 +143,7 @@ IMPORT_EXPORT_USE_TRANSATIONS = True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-#add field to user 
+#add field to user
 AUTH_USER_MODEL = 'accounts.CustomUser'
 
 # SESSION_TIMEOUT
@@ -177,3 +161,4 @@ EMAIL_HOST_USER = 'noreply.noisoifujifilm@gmail.com'
 EMAIL_HOST_PASSWORD = 'endoserv!ce2021'
 
 # django_heroku.settings(locals())
+
