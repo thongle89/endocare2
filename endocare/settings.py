afc92d5a990759629904d25941a44f3053e65914
thongle89 <thongle.aisolution@gmail.com>
2021-12-13 09:44:59 +0700

    databses

--- endocare/settings.py
+++ endocare/settings.py
@@ -75,12 +75,13 @@ WSGI_APPLICATION = 'endocare.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
+DATABASES = {
+       'default': {
+           'ENGINE': 'django.db.backends.sqlite3',
+           'NAME': BASE_DIR / 'db.sqlite3',
+       }
+    }
+
 
 DATABASES = {
      'default': {
@@ -91,7 +92,10 @@ DATABASES = {
          'HOST':'localhost',
          'PORT':'5432',
      }
- }
+}
+
+
+
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
