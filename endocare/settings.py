07ce1fcd88e4ed91ca30f0b5ec9f11cb0a41908c
thongle89 <thongle.aisolution@gmail.com>
2021-12-19 15:17:43 +0700

    change sqlite

--- endocare/settings.py
+++ endocare/settings.py
@@ -74,25 +74,25 @@ WSGI_APPLICATION = 'endocare.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-if DEBUG:
-    DATABASES = {
-           'default': {
-               'ENGINE': 'django.db.backends.sqlite3',
-               'NAME': BASE_DIR / 'db.sqlite3',
-           }
-        }
-else:
 
-    DATABASES = {
-         'default': {
-             'ENGINE': 'django.db.backends.postgresql',
-             'NAME': 'web_db',
-             'USER': 'web_user',
-             'PASSWORD':'nhakANDUkj81',
-             'HOST':'localhost',
-             'PORT':'5432',
-         }
+DATABASES = {
+       'default': {
+           'ENGINE': 'django.db.backends.sqlite3',
+           'NAME': BASE_DIR / 'db.sqlite3',
+       }
     }
+
+
+# DATABASES = {
+#      'default': {
+#          'ENGINE': 'django.db.backends.postgresql',
+#          'NAME': 'web_db',
+#          'USER': 'web_user',
+#          'PASSWORD':'nhakANDUkj81',
+#          'HOST':'localhost',
+#          'PORT':'5432',
+#      }
+# }
 
 
 
